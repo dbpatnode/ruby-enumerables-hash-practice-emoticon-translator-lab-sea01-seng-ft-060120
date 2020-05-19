e89eba3df60999d9aeaef2dd2e64a4bae0c0296c
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-19 20:20:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,13 @@ require 'yaml'
 
 def load_library(path)
   emoticons = YAML.load_file(path)
+  emoticons.each do |key, value|
   binding.pry
   # emoticons_from_yaml_file = {}
   # File.open("emoticons.yml") { |file| file.write(emoticons.to_yaml) }
   # binding.pry
   # emoticons_from_yaml_file = YAML.load(File.read("emoticons.yml"))
+end
 end
 
 def get_japanese_emoticon
