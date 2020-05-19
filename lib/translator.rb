7fcc57c05dd1ac11622a698cfe3674df9a121de0
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-19 21:27:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,28 +15,12 @@ end
 
 def get_japanese_emoticon(file_path, english_emoji)
   library = load_library("./lib/emoticons.yml")
-  binding.pry
+  
   library.each do |key, value|
-    binding.pry
     value[:japanese]
       if value[:english] == english_emoji
       end
-      def get_japanese_emoticon(file_path, eng_emo)
-  # code goes here
-  library = load_library('./lib/emoticons.yml')
-  library.each do |meaning, idioms|
-      return idioms[:japanese] if idioms[:english] == eng_emo
-  end
-  "Sorry, that emoticon was not found"
-end
-def get_english_meaning(file_path, jap_emo)
-  # code goes here
-  library = load_library('./lib/emoticons.yml')
-  library.each do |meaning, idioms|
-    return meaning if idioms[:japanese] == jap_emo
-  end
-    "Sorry, that emoticon was not found"
- end 
+      "Sorry, that emoticon was not found"
   end  
 end
 
